743fba4282e8bbb1808e6b704a769699a16e3254
Rabin <rabindc77@gmail.com>
2026-06-06 14:19:06 +0545

    2nd day hello

---
+++ Day02/hello.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>  // include the library or iostream which is header
+                    // file which includes the definations of the header functions
+using namespace std;   //Standard library ko names use garda std:: lekhna pardaina.
+/*Example:
+
+Without:
+std::cout << "Hello";
+std::endl;
+
+With:
+cout << "Hello";
+endl; */ 
+
+
+int main() /* compilation starts from main function */
+{
+// Display greeting message
+cout << "Hello";
+return 0; /* return statement to indicate successful execution */
+}
