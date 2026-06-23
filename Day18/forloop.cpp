177e54965f35a5c51534bab11503385acefade99
Rabin <rabindc77@gmail.com>
2026-06-23 22:47:21 +0545

    this is all abt. for loop

---
+++ Day18/forloop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Initialization: int i = 1
+    // Condition: i <= 5
+    // Update: i++
+    for (int i = 1; i <= 5; i++) {
+        cout << "Iteration number: " << i << endl;
+    }
+
+    return 0;
+}
