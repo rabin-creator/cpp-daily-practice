7b7c863e97383706da60327d8d936bd321f65fbe
Rabin <rabindc77@gmail.com>
2026-06-30 21:59:13 +0545

    the pointers

---
+++ Day25/pointers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 5, y = 10;
+    swap(&x, &y);
+    cout << "x = " << x << ", y = " << y;  // x = 10, y = 5
+}
