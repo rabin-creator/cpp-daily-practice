34cbdcb928a725be3507cdd689dae60cf8ca4f72
Rabin <rabindc77@gmail.com>
2026-07-09 21:53:02 +0545

    classes

---
+++ Day34/classes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+   public:
+      string name;
+      int age;
+
+      void introduce() {
+         cout << "My name is " << name << " and I am " << age << " years old." << endl;
+      }
+};
+
+int main() {
+   Student s1;          // Object creation
+   s1.name = "Rabin";
+   s1.age = 21;
+   s1.introduce();      // Function call
+   return 0;
+}
